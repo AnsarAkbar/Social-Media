0eeaba2228bfab9f7d657cb2180b9d3a3815b58b
AnsarAkbar <ansarmahmood298@gmail.com>
2024-11-25 22:54:01 +0500

    DB_connection

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,25 @@
-import express from 'express'
+import express from "express";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import dotenv from 'dotenv';
 
-const app=express()
-const port=3000;
 
-app.get("/",(req,res)=>{
-    res.json({name:'Ansar'})
-})
 
-app.listen(port,()=>console.log(`Server is running at 3000`))
+const app = express();
+const PORT = process.env.PORT || 3000;
+// Load environment variables from .env file
+dotenv.config();
+
+console.log(process.env.DB_NAME)
+// (async function () {
+//   try {
+//     const connectionString = `${process.env.DB_URI}/${DB_NAME}`;
+//     console.log("Connecting with URI: ", connectionString);
+//     await mongoose
+//       .connect(connectionString)
+//       .then(() => console.log("DB connected...!"))
+//       .catch((error) => console.log("Connection Failed:", error));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
