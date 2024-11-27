bc5f61b17357e8654cf4aef1cfee190551394f9f
AnsarAkbar <ansarmahmood298@gmail.com>
2024-11-27 19:57:37 +0500

    Stop tracking package.json and package-lock.json

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,91 +1,85 @@
 import mongoose from "mongoose";
-import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
-    {
-        userName: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-            index: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true,
-        },
-        fullName: {
-            type: String,
-            required: true,
-            trim: true,
-            index: true,
-        },
-        avatar: {
-            type: String,
-            required: true,
-        },
-        coverImage: {
-            type: String,
-        },
-        password: {
-            type: String,
-            required: [true, "Password is required...!"],
-            trim: true,
-        },
-        refreshToken: {
-            type: String,
-        },
-        watchHistory: {
-            type: [
-                {
-                    type: mongoose.Schema.Types.ObjectId,
-                    ref: "Video",
-                },
-            ],
-        },
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
     },
-    { timestamps: true }
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required...!"],
+      trim: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+    watchHistory: {
+      type: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Video",
+        },
+      ],
+    },
+  },
+  { timestamps: true }
 );
 
-
-
-userSchema.pre('save', function (next) {
-    if (this.isModified('password')) {
-        this.password = bcrypt.hash(this.password, 10)
-        next()
-    }
-})
+userSchema.pre("save", function (next) {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+    next();
+  }
+});
 
 userSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password)
-}
+  return await bcrypt.compare(password, this.password);
+};
 
-userSchema.methods.genrateAccessToken=function(){
-    return jwt.sign(
-        {
-            _id:this._id,
-            userName:this.useName,
-            fullName:this.fullName,
-            email:this.email,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        process.env.ACCESS_TOKEN_EXPIRE
-    )
-}
-userSchema.methods.genrateRefreshToken=function(){
-    jwt.sign(
-        {_id:this._id},
-        process.env.REFRESH_TOKEN_SECRET,
-        process.env.REFRESH_TOKEN_EXPIRE
-    )
-}
-
-
-
+userSchema.methods.genrateAccessToken = function () {
+  return jwt.sign(
+    //payload,secrateKey,option(expiresIn)
+    {
+      _id: this._id,
+      userName: this.useName,
+      fullName: this.fullName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRE }
+  );
+};
+userSchema.methods.genrateRefreshToken = function () {
+  jwt.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+  });
+};
 
 const User = mongoose.model("User", userSchema);
---
+++ Backend/src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { unlink } from "fs/promises";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET, // Click 'view API Keys' above to copy your API secret
+});
+
+export const uploadOnCloudinary = async (localImagePath) => {
+  try {
+    if (!localImagePath) return null;
+    // Upload an image
+    const response = await cloudinary.uploader.upload(localImagePath, {
+      public_id: "shoes",
+      resource_type: "auto",
+    });
+    console.log("image successfullfuly uploaded", response);
+    return response;
+  } catch (error) {
+    unlink(localImagePath); //delete the local file after the image has been uploaded successfully to Cloudinary
+    return null;
+  }
+};
